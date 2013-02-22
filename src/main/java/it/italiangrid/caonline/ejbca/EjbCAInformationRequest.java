c4f26a659c2e2efcd1fcd7a96bfdb4befc6b7383
dmichelotto <diego.michelotto@cnaf.infn.it>
2013-02-22 17:42:03 +0100

    Added last classes

---
+++ src/main/java/it/italiangrid/caonline/ejbca/EjbCAInformationRequest.java
@@ -0,0 +1,90 @@
+package it.italiangrid.caonline.ejbca;
+
+import it.italiangrid.caonline.model.CertificateInformation;
+import it.italiangrid.caonline.model.RevokeRequest;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.ejbca.core.protocol.ws.client.gen.AlreadyRevokedException_Exception;
+import org.ejbca.core.protocol.ws.client.gen.ApprovalException_Exception;
+import org.ejbca.core.protocol.ws.client.gen.AuthorizationDeniedException_Exception;
+import org.ejbca.core.protocol.ws.client.gen.CADoesntExistsException_Exception;
+import org.ejbca.core.protocol.ws.client.gen.Certificate;
+import org.ejbca.core.protocol.ws.client.gen.EjbcaException_Exception;
+import org.ejbca.core.protocol.ws.client.gen.EjbcaWS;
+import org.ejbca.core.protocol.ws.client.gen.IllegalQueryException_Exception;
+import org.ejbca.core.protocol.ws.client.gen.NotFoundException_Exception;
+import org.ejbca.core.protocol.ws.client.gen.UserDataVOWS;
+import org.ejbca.core.protocol.ws.client.gen.UserMatch;
+import org.ejbca.core.protocol.ws.client.gen.WaitingForApprovalException_Exception;
+
+/**
+ * 
+ * @author dmichelotto
+ *
+ */
+public class EjbCAInformationRequest {
+	
+	/**
+	 * Logger attribute.
+	 */
+	private static final Logger log = Logger.getLogger(EjbCACertificateRequest.class);
+	
+	/**
+	 * Ejbca Web Service.
+	 */
+	private EjbcaWS service;
+
+	/**
+	 * Contructor
+	 * 
+	 * @throws EjbCAException
+	 */
+
+	public EjbCAInformationRequest() throws EjbCAException {
+		log.error("Service initialization...");
+		try {
+			EjbcaWSConnection ejbcaWsConn = new EjbcaWSConnection();
+			this.service = ejbcaWsConn.getEjbcaWS();
+			log.error("Service successfully initialized.");
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new EjbCAException("Connetion problem");
+		}
+	}
+	
+	/**
+	 * List user certificates.
+	 * @param username - to search
+	 * @return list of certificate
+	 * @throws AuthorizationDeniedException_Exception
+	 * @throws EjbcaException_Exception
+	 * @throws IllegalQueryException_Exception
+	 * @throws CertificateException 
+	 */
+	public List<CertificateInformation> listCertificate(String username) throws AuthorizationDeniedException_Exception, EjbcaException_Exception, IllegalQueryException_Exception, CertificateException{
+		List<CertificateInformation> result = new ArrayList<CertificateInformation>();
+		
+		UserMatch userMatch = new UserMatch(UserMatch.MATCH_WITH_USERNAME,
+				UserMatch.MATCH_TYPE_CONTAINS, username);
+		
+		List<UserDataVOWS> findUsers = service.findUser(userMatch);
+		
+		for(UserDataVOWS user : findUsers){
+			List<Certificate> certs = service.findCerts(user.getUsername(), true);
+			for(Certificate cert :certs){
+				result.add(new CertificateInformation(user.getCertificateProfileName(), cert));
+			}
+		}
+		
+		return result;
+	}
+}
---
+++ src/main/java/it/italiangrid/caonline/model/CertificateInformation.java
@@ -0,0 +1,163 @@
+package it.italiangrid.caonline.model;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Date;
+
+import org.ejbca.core.protocol.ws.client.gen.Certificate;
+import org.ejbca.util.Base64;
+
+/**
+ * Class the display the information of the user certificate.
+ * @author dmichelotto
+ *
+ */
+public class CertificateInformation {
+	
+	/**
+	 * Certificate subject.
+	 */
+	private String subject;
+	
+	/**
+	 * Certificate issuer.
+	 */
+	private String issuer;
+	
+	/**
+	 * Certificate profile.
+	 */
+	private String profile;
+	
+	/**
+	 * Certificare creation date.
+	 */
+	private Date creationDate;
+	
+	/**
+	 * Certificare expiration date.
+	 */
+	private Date expirationDate;
+	
+	/**
+	 * 	The certificate.
+	 */
+	private String certificate;
+
+	/**
+	 * @param profile
+	 * @throws CertificateException 
+	 */
+	public CertificateInformation(String profile, Certificate c) throws CertificateException {
+		super();
+		
+		
+		CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+		InputStream in = new ByteArrayInputStream(c.getRawCertificateData());
+		X509Certificate cert = (X509Certificate)certFactory.generateCertificate(in);
+		
+		this.subject = cert.getSubjectDN().getName();
+		this.issuer = cert.getIssuerDN().getName();
+		this.profile = profile;
+		this.creationDate = cert.getNotBefore();
+		this.expirationDate = cert.getNotAfter();
+		this.certificate = cert.toString().replaceAll("\n", "<br/>");
+	}
+
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	/**
+	 * @return the issuer
+	 */
+	public String getIssuer() {
+		return issuer;
+	}
+
+	/**
+	 * @param issuer the issuer to set
+	 */
+	public void setIssuer(String issuer) {
+		this.issuer = issuer;
+	}
+
+	/**
+	 * @return the profile
+	 */
+	public String getProfile() {
+		return profile;
+	}
+
+	/**
+	 * @param profile the profile to set
+	 */
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	
+	/**
+	 * @return the creationDate
+	 */
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	/**
+	 * @param creationDate the creationDate to set
+	 */
+	public void setCreationDatee(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	/**
+	 * @return the expirationDate
+	 */
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+
+	/**
+	 * @param expirationDate the expirationDate to set
+	 */
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	/**
+	 * @return the certificate
+	 */
+	public String getCertificate() {
+		return certificate;
+	}
+
+	/**
+	 * @param certificate the certificate to set
+	 */
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "CertificateInformation [subject=" + subject + ", issuer="
+				+ issuer + ", profile=" + profile + ", expirationDate="
+				+ expirationDate + ", certificate=" + certificate + "]";
+	}
+}
